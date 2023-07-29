cee15d7b5a430689f1d5af2fd87c75da9ba3ca33
Rohulla123 <rohullah.asify@gmail.com>
2023-07-29 11:37:06 -0400

    initiated commit

---
+++ src/TeamProject/DemoTeamProject.java
@@ -0,0 +1,20 @@
+package TeamProject;
+
+public class DemoTeamProject {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Test3");
+	}
+	// add method
+	public static void add( int a, int b) {
+		
+	}
+
+	// substract method
+	public static void substract( int a, int b) {
+		System.out.println(a-b);
+	}
+	
+
+}
